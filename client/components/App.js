64b42194c10116eef15416ff01097ec0d9f689fd
Michael Kulinski <me@michaelkulinski.com>
2016-12-14 16:53:01 -0800

    parsed XML to JSON and cleaned up the title and description string to only include the needed elements.

--- client/components/App.js
+++ client/components/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
   constructor() {
     super();
-    this.state = { markers: [] };
+    this.state = { buoys: [] };
+  }
+  componentDidMount() {
+    axios.get('/allBuoys')
+    .then((data) => {
+      console.log(data.data);
+      const arr = data.data.map((item) => {
+        return [item.title, item.description];
+      });
+      this.setState({ buoys: arr });
+    });
   }
 
   render() {
     return (
       <div>
-        Main App
+        { this.state.buoys }
       </div>
     );
   }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const http = require('http');
+const X2JS = require('x2js');
 
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
+const buoyController = require('./controllers/buoyController');
 // const userController = require('./controllers/userController');
 
 const mongoURI = 'mongodb://localhost:27017/buoy';
@@ -14,6 +17,67 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../client')));
 
+// $.get(URL, function (data) {
+//     $(data).find("item").each(function () { // or "item" or whatever suits your feed
+//         var el = $(this);
+//
+//         console.log("------------------------");
+//         console.log("title      : " + el.find("title").text());
+//         console.log("description: " + el.find("description").text());
+//     });
+// });
+
+const options = {
+  host: 'www.ndbc.noaa.gov',
+  path: '/rss/ndbc_obs_search.php?lat=40N&lon=73W&radius=100',
+  method: 'GET',
+};
+
+let doc;
+
+function callback(response) {
+  let str = '';
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  response.on('end', function() {
+    const x2js = new X2JS();
+    const document = x2js.xml2js(str);
+    doc = document.rss.channel.item;
+    doc.forEach((item) => {
+      item.description.replace(/\/|br|<|>|\n|:/ig, '');
+    })
+    let test = doc[0].description.replace(/\/|br|<|>|\n|:/ig, '');
+    test = test.split('strong').map((item) => {
+      return item.trim();
+    });
+    const date = test.splice(0, 3).filter((item) => item !== '');
+    const outObj = {};
+    for (let i = 0; i < test.length; i++) {
+      if (!i % 2) {
+        outObj[test[i]];
+      } else {
+        outObj[test[i - 1]] = test[i];
+      }
+    }
+    buoyController.addBuoy({ })
+    console.log(outObj);
+
+    // console.log(document.rss.channel.item);
+    // const title = document.rss.channel.item[0].title;
+    // console.log(title)
+    // const description = document.rss.channel.item[0].description;
+    // console.log(description);
+  });
+}
+
+const requesting = http.request(options, callback);
+requesting.end();
+
+app.get('/allBuoys', (req, res) => {
+  res.json(doc);
+});
 
 app.listen(3000, () => {
   console.log('server running');
